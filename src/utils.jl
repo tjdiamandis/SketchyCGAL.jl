deb8098d856111d38467f9d3367060e41a6819c2
Theo Diamandis <tjdiamandis@gmail.com>
2021-05-23 14:26:52 -0400

    add printing

---
+++ src/utils.jl
@@ -0,0 +1,37 @@
+# Adapted from 
+# https://github.com/ZIB-IOL/FrankWolfe.jl/blob/master/src/fw_algorithms.jl
+function print_header(data)
+    @printf(
+        "\n─────────────────────────────────────────────────────────────────────────────────────────────────\n"
+    )
+    @printf(
+        "%13s %14s %14s %14s %14s %14s\n",
+        data[1],
+        data[2],
+        data[3],
+        data[4],
+        data[5],
+        data[6]
+    )
+    @printf(
+        "─────────────────────────────────────────────────────────────────────────────────────────────────\n"
+    )
+end
+
+function print_footer()
+    @printf(
+        "─────────────────────────────────────────────────────────────────────────────────────────────────\n\n"
+    )
+end
+
+function print_iter_func(data)
+    @printf(
+        "%13s %14e %14e %14e %14e %13.1f\n",
+        data[1],
+        Float64(data[2]),
+        Float64(data[3]),
+        Float64(data[4]),
+        Float64(data[5]),
+        data[6]
+    )
+end
