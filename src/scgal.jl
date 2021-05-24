1801cef429f7d3c1dc2139e48977691f312fa802
Theo Diamandis <tjdiamandis@gmail.com>
2021-05-24 01:29:20 -0400

    full version working

--- src/scgal.jl
+++ src/scgal.jl
@@ -1,5 +1,5 @@
 # No primatives
-function scgal_dense(
+function scgal_full(
         C_input,
         b_input,
         A!,
@@ -32,10 +32,13 @@ function scgal_dense(
     yt = zeros(d)
     zt = zeros(d)
 
+    qmax = min(n-1, Int(ceil(max_iters^0.25 * log(n))))
     cache = (
         A_X = zeros(d),
         dual_update = zeros(d),
-        sketch_update = zeros(R)
+        sketch_update = zeros(R),
+        evec=init_cache_evec(n, qmax),
+        tmp = zeros(n)
     )
     Dt = copy(C)
     v = zeros(n)
@@ -63,8 +66,8 @@ function scgal_dense(
 
         # --- Eigenvector computation ---
         # * Custom Method *
-        q = Int(ceil(10 * t^0.25 * log(n)))
-        ξ, v = approx_min_evec(Dt, n=n, q=q)
+        q = Int(ceil(t^0.25 * log(n)))
+        ξ = approx_min_evec!(v, Dt, n=n, q=q, cache=cache.evec)
         # * ArnoldiMethod.jl (more stable than Lanzcos) *
         # F = partialschur(Dt, nev=1, which=SR(), tol=sqrt(n)*eps())
         # !F[2].converged && error("Eigvec computation did not converge")
@@ -75,7 +78,145 @@ function scgal_dense(
         # --- "Primal" update ---
         zt .-= η.*zt
         # TODO: use primative (3)
+        # NOTE: This only works for MAXCUT
         zt .+= η .* v .* v
+
+
+        # --- Sketch update ---
+        # St = (1-η)*St + η*v*v'*Ω
+        rank_one_update!(St, Ω, v, η; cache=cache.sketch_update)
+
+
+        # --- Dual update (& primal infeasibility) ---
+        cache.dual_update .= zt .- b
+        # eq (6.4)
+        primal_infeas = norm(cache.dual_update)
+        γ = min(β0, α^2*βt*η^2 / primal_infeas^2)
+        primal_infeas /= (scale_X * (1 + norm_b))
+        @. yt += γ*cache.dual_update
+
+
+        # --- compute objective values for output ---
+        mul!(cache.tmp, C, v)
+        obj_val = (1-η)*obj_val + η*dot(v, cache.tmp)
+        dual_val = dot(b, yt) * 1/scale_X
+
+
+        # --- duality gap ---
+        @. cache.dual_update = yt + βt*cache.dual_update
+        dual_gap = obj_val + dot(cache.dual_update, cache.A_X) - ξ[1]
+
+
+        # --- printing ---
+        if t == 1 || t % print_iter == 0
+            print_iter_func((
+                string(t),
+                obj_val * 1/scale_C * 1/scale_X,
+                dual_val,
+                dual_gap,
+                primal_infeas,
+                (time_ns() - time_start) / 1e9
+            ))
+        end
+        t += 1
+    end
+
+    # --- Prepare Output ---
+    solve_time = (time_ns() - time_start) / 1e9
+    # Reconstruct Xhat = U*Λ*U'
+    U, Λ = reconstruct(Ω, St)
+
+    correction_factor = (1.0 - tr(Λ))/R
+    # Λ.diag .= Λ.diag .+ correction_factor
+
+    # TODO: return a soution object
+    # primal var (U, Λ), dual var, AX
+    return U, Λ, St, Ω, yt, zt
+
+end
+
+
+
+# Uses primatives defined in (2.4) in the paper
+function scgal(
+        C_u!,           # u     -> C*u
+        Aadj_zu!,       # (u,z) -> A_adj(z)*u
+        A_uu!,          # u     -> A(u*u')
+        b_input,
+        n,
+        d,
+        R = 30,
+        scale_X = 1,
+        scale_C = 1,
+        max_iters = 1_000,
+        tol = 1e-10,
+        print_iter = 10,
+)
+
+    # Copy input
+    b = copy(b_input) .* scale_X
+    norm_b = norm(b_input)
+
+    α = 1
+    t = 1
+    β0 = 1
+    K = Inf
+
+    dual_gap = Inf
+    obj_val = 0
+
+    Ω, St = init_sketch(n, R)
+    yt = zeros(d)
+    zt = zeros(d)
+
+    # TODO: maybe better to rename to indicate size?
+    qmax = min(n-1, Int(ceil(max_iters^0.25 * log(n))))
+    cache = (
+        A_X = zeros(d),
+        dual_update = zeros(d),
+        sketch_update = zeros(R),
+        evec = init_cache_evec(n, qmax),
+        tmp = zeros(n)
+    )
+    Dt = copy(C)
+    v = zeros(n)
+
+    # Keep track of things (credit: https://github.com/ZIB-IOL/FrankWolfe.jl)
+    headers = ["Iteration", "Primal", "Dual", "Dual Gap", "Infeas", "Time"]
+    print_header(headers)
+    time_start = time_ns()
+    while t <= max_iters #&& dual_gap >= max(tol, eps())
+        # --- Parameter updates (from Yurtsever et al Alg 6.1) ---
+        βt = β0 * sqrt(t + 1)
+        η = 2/(t + 1)
+
+
+        # --- Gradient computation ---
+        # * Non-allocating *
+        cache.A_X .= zt .- b
+        cache.A_X .*= βt
+        cache.A_X .+= yt
+        # C entries are already in Dt -- just update diagonal
+        # for i in 1:n
+        #     Dt[i,i] = C[i,i] + cache.A_X[i]
+        # end
+
+
+        # --- Eigenvector computation ---
+        # * Custom Method *
+        q = Int(ceil(10 * t^0.25 * log(n)))
+        ξ = approx_min_evec!(v, C_u!, Aadj_zu!, cache.AX, n=n, q=q, cache=cache.evec)
+        # * ArnoldiMethod.jl (more stable than Lanzcos) *
+        # F = partialschur(Dt, nev=1, which=SR(), tol=sqrt(n)*eps())
+        # !F[2].converged && error("Eigvec computation did not converge")
+        # ξ, vv = partialeigen(F[1])
+        # v .= vv[:,1]
+
+
+        # --- "Primal" update ---
+        zt .-= η.*zt
+        A_uu!(cache.dual_update, v)
+        zt .+= η .* cache.dual_update
 
 
         # --- Sketch update ---
@@ -91,7 +232,9 @@ function scgal_dense(
 
         # --- compute objective values for output ---
         # TODO: make this efficient using primative 1
-        obj_val = (1-η)*obj_val + η*v'*C*v
+        C_u!(cache.tmp, v)
+        obj_update = dot(v, cache.tmp)
+        obj_val = (1-η)*obj_val + η*obj_update
         dual_val = dot(b, yt) * 1/scale_X
 
 
@@ -116,7 +259,8 @@ function scgal_dense(
     # --- Prepare Output ---
     solve_time = (time_ns() - time_start) / 1e9
     # Reconstruct Xhat = U*Λ*U'
-    U, Λ = reconstruct(Ω, S)
+    U, Λ = reconstruct(Ω, St)
+    @. Λ.diag += (1.0 - tr(Λ))*ones(R) / R
 
     # TODO: return a soution object
     # primal var (U, Λ), dual var, AX
