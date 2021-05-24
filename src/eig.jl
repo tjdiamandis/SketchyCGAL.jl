0afcec6dfb0c26874fe472eac809b59a7f9880f0
Theo Diamandis <tjdiamandis@gmail.com>
2021-05-24 01:28:40 -0400

    using primatives

--- src/eig.jl
+++ src/eig.jl
@@ -63,7 +63,7 @@ function approx_min_evec(M; n::Int, q::Int, cache=nothing, tol=1e-12)
     cache.tmp1[1:i] .= eigvecs(T)[:,1]
     mul!(cache.tmp2, @view(cache.V[:,1:i]), @view(cache.tmp1[1:i]))
     mul!(cache.tmp1, M, cache.tmp2)
-    ξ = dot(v, cache.tmp1)
+    ξ = dot(cache.tmp2, cache.tmp1)
     return ξ, cache.tmp2
 
 end
@@ -157,6 +157,58 @@ function approx_min_evec!(v, M!; n::Int, q::Int, cache=nothing, tol=1e-12)
     # TODO: make this line unallocating
     cache.tmp1[1:i] .= eigvecs(T)[:,1]
     @views mul!(v, cache.V[:,1:i], cache.tmp1[1:i])
+    M!(cache.tmp1, v)
+    ξ = dot(v, cache.tmp1)
+    return ξ
+
+end
+
+
+# Less allocating, directly uses two primatives
+function approx_min_evec!(v, C_u!, Aadj_zu!, AX; n::Int, q::Int, cache=nothing, tol=1e-12)
+    max_iters = min(q, n-1)
+    if isnothing(cache)
+        cache = init_cache_evec(n, max_iters)
+    end
+
+    #initialize
+    @views randn!(cache.V[:,1])
+    @views cache.V[:,1] .= cache.V[:,1] ./ norm(cache.V[:,1])
+
+    i = 1
+    while i <= max_iters
+        vi = @view(cache.V[:,i])
+        vi1 = @view(cache.V[:,i+1])
+
+        C_u!(cache.tmp1, vi)
+        Aadj_zu!(vi1, AX, vi)
+        vi1 .+= cache.tmp1
+        cache.w[i] = dot(vi, vi1)
+
+        vi1 .-= cache.w[i] .* vi
+        if i > 1
+            @views vi1 .-= cache.p[i-1] .* cache.V[:,i-1]
+        end
+
+        cache.p[i] = norm(vi1)
+        cache.p[i] < tol && break
+
+        vi1 ./= cache.p[i]
+        i += 1
+
+    end
+    i -= 1
+
+    @views T = SymTridiagonal(cache.w[1:i], cache.p[1:i-1])
+    # LAPACK ZLARRV for tridiagonal eigenvalues
+    # TODO: make this line unallocating
+    cache.tmp1[1:i] .= eigvecs(T)[:,1]
+    @views mul!(v, cache.V[:,1:i], cache.tmp1[1:i])
+
+    C_u!(cache.tmp1, v)
+    Aadj_zu!(cache.tmp2, AX, v)
+    cache.tmp1 .+= cache.tmp2
+
     M!(cache.tmp1, v)
     ξ = dot(v, cache.tmp1)
     return ξ
