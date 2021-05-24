65016fde2d7ab6f231dbd356c85e288bdd5c2611
Theo Diamandis <tjdiamandis@gmail.com>
2021-05-24 17:08:26 -0400

    add log types

--- src/types.jl
+++ src/types.jl
@@ -1,4 +1,6 @@
 abstract type SCGALResults end
+abstract type OptimizationLog end
+
 struct SCGALSolution{T<:Real} <: SCGALResults
     UT::AbstractMatrix{T}
     ΛT::AbstractMatrix{T}
@@ -6,5 +8,15 @@ struct SCGALSolution{T<:Real} <: SCGALResults
     Ω::AbstractMatrix{T}
     yT::AbstractVector{T}
     zT::AbstractVector{T}
-    log
+    log::Union{OptimizationLog, Nothing}
+end
+
+
+struct SCGALLog{T<:Real} <: OptimizationLog
+    dual_gap::AbstractVector{T}
+    obj_val::AbstractVector{T}
+    primal_infeas::AbstractVector{T}
+    time_sec::AbstractVector{T}
+    obj_val_Xhat::Union{AbstractVector{T}, Nothing}
+    primal_infeas_Xhat::Union{AbstractVector{T}, Nothing}
 end
